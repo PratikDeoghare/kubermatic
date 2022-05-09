1030ee28c38be159967bd2fb399e66ae40c3098f
Pratik Deoghare <pratik.deoghare@gmail.com>
2022-05-09 06:30:36 +0200

    .

--- pkg/test/dualstack/azure_dualstack_test.go
+++ pkg/test/dualstack/azure_dualstack_test.go
@@ -1,12 +1,34 @@
+//go:build dualstack
+
+/*
+Copyright 2022 The Kubermatic Kubernetes Platform contributors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package dualstack
 
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
 	"github.com/kubermatic/machine-controller/pkg/cloudprovider/util"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/project"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/models"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -17,11 +39,6 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/utils/pointer"
-	"net/http"
-	"os"
-	"sync"
-	"testing"
-	"time"
 )
 
 const (
